0b49dfdf517670695593d25b7f4f4d18198c5f74
Henrriegel <luis.enrique.gr.legr@outlook.com>
2022-07-21 03:19:29 -0500

    Proyecto terminado

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,292 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+<div class="grid-container">
+
+  <div class="formView">
+    <form action="" class="form" @submit.prevent="formSubmit">
+
+      <h1 class="formTitle" style="width: 100%; font-size: 35px; text-align: center;">Formulario</h1> 
+
+      <div class="form-group">
+        <input v-model="creditForm.valorPresente" type="text" name="valorPresente" required />
+        <span class="highlight"></span>
+        <span class="bar"></span>
+        <label for="valorPresente">Valor Presente (P)</label>
+      </div>
+
+      <div class="input-group">
+        <div class="form-group">
+          <input v-model="creditForm.plazo" type="text" name="plazo" required />
+          <span class="highlight"></span>
+          <span class="bar"></span>
+          <label for="plazo">Plazo (n)</label>
+        </div>
+
+        <select v-model="creditForm.tiempoPlazo" name="tiempoPlazo" id="tiempoPlazo" placeholder="Seleccione tiempo"  style="height: 25px; width:fit-content; transform: translateX(-75%);" required>
+            <option disabled value="">Elija una:</option>
+            <option value="Meses">Meses</option>
+            <option value="Años">Años</option>
+        </select>
+      </div>
+
+      <div class="input-group">
+        <div class="form-group">
+          <input v-model="creditForm.tasaInteres" type="text" name="tasaInteres" required />
+          <span class="highlight"></span>
+          <span class="bar"></span>
+          <label for="tasaInteres">Tasa de interés (i) en porcentaje</label>
+        </div>
+
+        <select v-model="creditForm.interesTiempo" name="interesTiempo" id="interesTiempo" placeholder="Seleccione tiempo"  style="height: 25px; width:fit-content; transform: translateX(-75%);" required>
+            <option disabled value="">Elija una:</option>
+            <option value="Mensual">Mensual</option>
+            <option value="Anual">Anual</option>
+        </select>
+
+        <p style="height: 25px; width:fit-content; transform: translateX(-65%);">capitalizable</p>
+
+        <select v-model="creditForm.capitalizable" name="capitalizable" id="capitalizable" placeholder="Seleccione tiempo"  style="height: 25px; width:fit-content; transform: translateX(-40%);" required>
+            <option disabled value="">Elija una:</option>
+            <option value="Mensualmente">Mensualmente</option>
+            <option value="Anualmente">Anualmente</option>
+        </select>
+      </div>
+
+      <button class="b1" type="submit" style="margin: 0% 30% 3% 30%">Generar ciclo de vida del crédito</button>
+    </form>
+    <div>
+      <text style="color:blue; font-size: 25px; text-align: center; width: 100%;">La Formula de la anualidad es:</text> <br>
+      <img src="@/assets/formula.png" alt=""> <br>
+      <text style="color:blue; font-size: 25px; text-align: center; width: 100%;" v-if="success">A = {{this.a}}</text> <br>
+      <button @click="exportTable()" v-if="success" style="margin: 15px; padding: 5px;">Export table to excel</button>
+      <text style="color:red; font-size: 25px; text-align: center; width: 100%;" >{{this.formError}}</text>
+    </div>
+    <div class="formula">
+    </div>
+  </div>
+
+  <div class="lifeCycle">
+    <table border="1" id="lifeCycleTable" ref="exp_table">
+      <thead>
+        <tr>
+          <th>Periodo</th>
+          <th>Saldo</th>
+          <th>Interés</th>
+          <th>IVA</th>
+          <th>Abono a capital</th>
+          <th>Pago Mensual</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>0</td>
+          <td>{{this.P}}</td>
+          <td>-</td>
+          <td>-</td>
+          <td>-</td>
+          <td>-</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</div>
 </template>
+<script>
+  import 'table2excel'
+  export default {
+    data() {
+        return {
+            tableData: [],
+            tableColumns: [],
+            formError: "",
+            creditForm: {
+                valorPresente: "",
+                plazo: "",
+                tiempoPlazo: "",
+                tasaInteres: "",
+                interesTiempo: "",
+                capitalizable: ""
+            },
+            a: 0,
+            n: 0,
+            i: 0,
+            P: 0,
+            P_anterior: 0,
+            success: false
+        };
+    },
+    methods: {
+      formSubmit() {
+          //Si capitalizable mensualmente significa que los pagos serán mensuales
+          this.P = parseFloat(this.creditForm.valorPresente);
+          this.n = parseFloat(this.creditForm.plazo);
+          this.i = parseFloat(this.creditForm.tasaInteres) / 100;
+          if (isNaN(this.P) || isNaN(this.n) || isNaN(this.i)) {
+              this.formError = "El formulario se llenó mal";
+              this.erraseForm();
+          }
+          else {
+              if (this.creditForm.capitalizable == "Mensualmente") {
+                  if (this.creditForm.tiempoPlazo == "Años")
+                      this.n *= 12;
+                  if (this.creditForm.interesTiempo == "Anual")
+                      this.i /= 12;
+              }
+              else {
+                  if (this.creditForm.tiempoPlazo == "Meses")
+                      this.n /= 12;
+                  if (this.creditForm.interesTiempo == "Mensual")
+                      this.i *= 12;
+              }
+              this.a = (this.P * this.i * 1.16) / (1 - (1 / ((1 + this.i * 1.16) ** this.n)));
+              this.fillTable();
+              this.success = true;
+          }
+      },
+      fillTable() {
+        let table = document.getElementById("lifeCycleTable");
+        let lastRow = 0;
+        this.P_anterior = this.P;
+        for (let i = 1; i <= this.n; i++) {
+            lastRow = table.rows.length;
+            let rowInsert = table.insertRow(lastRow);
+            let colPeriodo = rowInsert.insertCell(0);
+            let colSaldo = rowInsert.insertCell(1);
+            let colInteres = rowInsert.insertCell(2);
+            let colIVA = rowInsert.insertCell(3);
+            let colAbono = rowInsert.insertCell(4);
+            let colPago = rowInsert.insertCell(5);
+            colPeriodo.innerHTML = i;
+            colInteres.innerHTML = (this.P_anterior * this.i).toFixed(2);
+            colIVA.innerHTML = (this.P_anterior * this.i * 0.16).toFixed(2);
+            colAbono.innerHTML = (this.a - this.P_anterior * this.i - this.P_anterior * this.i * 0.16).toFixed(2);
+            colPago.innerHTML = (this.a).toFixed(2);
+            this.P_anterior -= this.a - this.P_anterior * this.i - this.P_anterior * this.i * 0.16;
+            colSaldo.innerHTML = (this.P_anterior).toFixed(2);
+        }
+        this.tableData = table.tHead.innerHTML
+        this.tableColumns = table.tBodies[0].innerHTML
+      },
+      exportTable() {
+        var table2excel = new Table2Excel();
+        table2excel.export(document.querySelectorAll("#lifeCycleTable"));
+      },
+      erraseForm() {
+          this.creditForm.valorPresente = "";
+          this.creditForm.plazo = "";
+          this.creditForm.tiempoPlazo = "";
+          this.creditForm.tasaInteres = "";
+          this.creditForm.interesTiempo = "";
+          this.creditForm.capitalizable = "";
+      }
+    },
+    components: { 
+
+    }
+  }
+</script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+.grid-container {
+  display: grid;
+  height: 100%;
+  width: 100%;
+  grid-template-columns: 50% 50%;
+}
+.grid-item {
+  padding: 15%
+}
+.formView {
+  width: 100%;
+  min-width: 620px;
+  padding: 20% 5% 20% 5%;
+  display: flex;
+  flex-direction: column;
+  align-items:center;
+}
+.lifeCycle{
+  display: flex;
+  flex-direction: column;
+  align-items:center;
+  margin: 5% 10% 0% 10%;
+}
+table {
+  width: 100%;
+}
+form {
+  border-style: solid;
+  border-radius: 25px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+.form-group {
+	position: relative;
+	margin-bottom: 25px;
+  padding-left: 15px;
+
+}
+.input-group{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+input {
+	display: block;
+	font-size: 14pt;
+	padding: 10px;
+	border: none;
+	border-bottom: 1px solid #ccc;
 }
 
-nav {
-  padding: 30px;
+input:focus {
+	outline: none;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+label {
+	position: absolute;
+	top: 10px;
+	left: 5px;
+	color: #999;
+	font-size: 14pt;
+	font-weight: normal;
+	pointer-events: none;
+	transition: all 0.2s ease;
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
+input:focus ~ label,
+input:valid ~ label {
+	top: -20px;
+	font-size: 10pt;
+	color: #5264AE;
+}
+
+.bar {
+	display: block;
+	position: relative;
+	width: 320px;
+}
+
+.bar:before,
+.bar:after {
+	content: "";
+	height: 2px;
+	width: 0;
+	bottom: 1px;
+	position: absolute;
+	background: #5264AE;
+	transition: all 0.2s ease;
+}
+
+.bar:before {
+	left: 50%;
+}
+
+.bar:after {
+	right: 50%;
+}
+
+input:focus ~ .bar:before,
+input:focus ~ .bar:after {
+	width: 50%;
 }
 </style>
