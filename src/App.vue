1b5de8bf15eeb547607f71b31ab705c075141745
Henrriegel <luis.enrique.gr.legr@outlook.com>
2022-07-25 00:47:41 -0500

    Optimized

--- src/App.vue
+++ src/App.vue
@@ -22,9 +22,9 @@
         </div>
 
         <select v-model="creditForm.tiempoPlazo" name="tiempoPlazo" id="tiempoPlazo" placeholder="Seleccione tiempo"  style="height: 25px; width:fit-content; transform: translateX(-75%);" required>
-            <option disabled value="">Elija una:</option>
-            <option value="Meses">Meses</option>
-            <option value="Años">Años</option>
+          <option disabled value="">Elija una:</option>
+          <option value="Meses">Meses</option>
+          <option value="Años">Años</option>
         </select>
       </div>
 
@@ -37,17 +37,17 @@
         </div>
 
         <select v-model="creditForm.interesTiempo" name="interesTiempo" id="interesTiempo" placeholder="Seleccione tiempo"  style="height: 25px; width:fit-content; transform: translateX(-75%);" required>
-            <option disabled value="">Elija una:</option>
-            <option value="Mensual">Mensual</option>
-            <option value="Anual">Anual</option>
+          <option disabled value="">Elija una:</option>
+          <option value="Mensual">Mensual</option>
+          <option value="Anual">Anual</option>
         </select>
 
         <p style="height: 25px; width:fit-content; transform: translateX(-65%);">capitalizable</p>
 
         <select v-model="creditForm.capitalizable" name="capitalizable" id="capitalizable" placeholder="Seleccione tiempo"  style="height: 25px; width:fit-content; transform: translateX(-40%);" required>
-            <option disabled value="">Elija una:</option>
-            <option value="Mensualmente">Mensualmente</option>
-            <option value="Anualmente">Anualmente</option>
+          <option disabled value="">Elija una:</option>
+          <option value="Mensualmente">Mensualmente</option>
+          <option value="Anualmente">Anualmente</option>
         </select>
       </div>
 
@@ -65,7 +65,7 @@
   </div>
 
   <div class="lifeCycle">
-    <table border="1" id="lifeCycleTable" ref="exp_table">
+    <table id="lifeCycleTable" ref="exp_table">
       <thead>
         <tr>
           <th>Periodo</th>
@@ -94,89 +94,79 @@
   import 'table2excel'
   export default {
     data() {
-        return {
-            tableData: [],
-            tableColumns: [],
-            formError: "",
-            creditForm: {
-                valorPresente: "",
-                plazo: "",
-                tiempoPlazo: "",
-                tasaInteres: "",
-                interesTiempo: "",
-                capitalizable: ""
-            },
-            a: 0,
-            n: 0,
-            i: 0,
-            P: 0,
-            P_anterior: 0,
-            success: false
-        };
+      return {
+        tableData: [],
+        tableColumns: [],
+        formError: "",
+        creditForm: {
+          valorPresente: "",
+          plazo: "",
+          tiempoPlazo: "",
+          tasaInteres: "",
+          interesTiempo: "",
+          capitalizable: ""
+        },
+        a: 0,
+        n: 0,
+        i: 0,
+        P: 0,
+        success: false
+      };
     },
     methods: {
       formSubmit() {
-          //Si capitalizable mensualmente significa que los pagos serán mensuales
-          this.P = parseFloat(this.creditForm.valorPresente);
-          this.n = parseFloat(this.creditForm.plazo);
-          this.i = parseFloat(this.creditForm.tasaInteres) / 100;
-          if (isNaN(this.P) || isNaN(this.n) || isNaN(this.i)) {
-              this.formError = "El formulario se llenó mal";
-              this.erraseForm();
+        this.P = parseFloat(this.creditForm.valorPresente);
+        this.n = parseFloat(this.creditForm.plazo);
+        this.i = parseFloat(this.creditForm.tasaInteres) / 100;
+        if (isNaN(this.P) || isNaN(this.n) || isNaN(this.i)) {
+          this.formError = "El formulario se llenó mal";
+          this.erraseForm();
+        } else {
+          if (this.creditForm.capitalizable == "Mensualmente") {
+            if (this.creditForm.tiempoPlazo == "Años") this.n *= 12;
+            if (this.creditForm.interesTiempo == "Anual") this.i /= 12;
+          } else {
+            if (this.creditForm.tiempoPlazo == "Meses") this.n /= 12;
+            if (this.creditForm.interesTiempo == "Mensual") this.i *= 12;
           }
-          else {
-              if (this.creditForm.capitalizable == "Mensualmente") {
-                  if (this.creditForm.tiempoPlazo == "Años")
-                      this.n *= 12;
-                  if (this.creditForm.interesTiempo == "Anual")
-                      this.i /= 12;
-              }
-              else {
-                  if (this.creditForm.tiempoPlazo == "Meses")
-                      this.n /= 12;
-                  if (this.creditForm.interesTiempo == "Mensual")
-                      this.i *= 12;
-              }
-              this.a = (this.P * this.i * 1.16) / (1 - (1 / ((1 + this.i * 1.16) ** this.n)));
-              this.fillTable();
-              this.success = true;
-          }
+          this.a = (this.P * this.i * 1.16) / (1 - (1 / ((1 + this.i * 1.16) ** this.n)));
+          this.fillTable();
+          this.success = true;
+        }
       },
       fillTable() {
         let table = document.getElementById("lifeCycleTable");
-        let lastRow = 0;
-        this.P_anterior = this.P;
+        let saldo_anterior = this.P;
         for (let i = 1; i <= this.n; i++) {
-            lastRow = table.rows.length;
-            let rowInsert = table.insertRow(lastRow);
-            let colPeriodo = rowInsert.insertCell(0);
-            let colSaldo = rowInsert.insertCell(1);
-            let colInteres = rowInsert.insertCell(2);
-            let colIVA = rowInsert.insertCell(3);
-            let colAbono = rowInsert.insertCell(4);
-            let colPago = rowInsert.insertCell(5);
-            colPeriodo.innerHTML = i;
-            colInteres.innerHTML = (this.P_anterior * this.i).toFixed(2);
-            colIVA.innerHTML = (this.P_anterior * this.i * 0.16).toFixed(2);
-            colAbono.innerHTML = (this.a - this.P_anterior * this.i - this.P_anterior * this.i * 0.16).toFixed(2);
-            colPago.innerHTML = (this.a).toFixed(2);
-            this.P_anterior -= this.a - this.P_anterior * this.i - this.P_anterior * this.i * 0.16;
-            colSaldo.innerHTML = (this.P_anterior).toFixed(2);
+          
+          let interes = saldo_anterior * this.i;
+          let iva = interes*.16;
+          let abono = this.a - interes - iva;
+          saldo_anterior -= abono;
+
+          let rowInsert = table.insertRow(table.rows.length);
+
+          rowInsert.insertCell(0).innerHTML = i;
+          rowInsert.insertCell(1).innerHTML = saldo_anterior.toFixed(2);
+          rowInsert.insertCell(2).innerHTML = (interes).toFixed(2);
+          rowInsert.insertCell(3).innerHTML = (iva).toFixed(2);
+          rowInsert.insertCell(4).innerHTML = (abono).toFixed(2);
+          rowInsert.insertCell(5).innerHTML = (this.a).toFixed(2);
         }
-        this.tableData = table.tHead.innerHTML
-        this.tableColumns = table.tBodies[0].innerHTML
+        this.tableData = table.tHead.innerHTML;
+        this.tableColumns = table.tBodies[0].innerHTML;
       },
       exportTable() {
         var table2excel = new Table2Excel();
         table2excel.export(document.querySelectorAll("#lifeCycleTable"));
       },
       erraseForm() {
-          this.creditForm.valorPresente = "";
-          this.creditForm.plazo = "";
-          this.creditForm.tiempoPlazo = "";
-          this.creditForm.tasaInteres = "";
-          this.creditForm.interesTiempo = "";
-          this.creditForm.capitalizable = "";
+        this.creditForm.valorPresente = "";
+        this.creditForm.plazo = "";
+        this.creditForm.tiempoPlazo = "";
+        this.creditForm.tasaInteres = "";
+        this.creditForm.interesTiempo = "";
+        this.creditForm.capitalizable = "";
       }
     },
     components: { 
@@ -185,7 +175,7 @@
   }
 </script>
 
-<style>
+<style> 
 .grid-container {
   display: grid;
   height: 100%;
@@ -211,7 +201,28 @@
 }
 table {
   width: 100%;
+  border-collapse:separate;
+  background: linear-gradient(-90deg,rgb(255, 54, 54), rgb(255, 206, 102));
+  border-radius:25px 25px 0px 0px;
 }
+
+table tbody tr 
+{
+  background: white;
+  border-bottom: 1px solid;
+  text-align: center;
+}
+/* table tbody tr:nth-of-type(even)
+{
+    background-color: rgb(241, 241, 241);
+}  */
+table td, table th 
+{
+  padding-top: 12px;
+  padding: 12px;
+  border-spacing: 0px 15px;
+}
+
 form {
   border-style: solid;
   border-radius: 25px;
